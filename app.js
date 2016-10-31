009ad252de6356df130bb4c9e082a3eb8d3d12be
Adrian Maseda <amaseda@gmail.com>
2016-10-31 16:24:39 -0400

    full crud

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const app = express()
 const Url = require("./db/models").Url
 const bodyParser = require("body-parser")
 const checksum = require("checksum")
+const methodOverride = require("method-override")
 
 app.set("view engine", "hbs")
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+app.use(methodOverride((req, res) => {
+  console.log("hello")
+  if("_method" in req.query){
+    return req.query._method
+  }
+}, {methods: ["POST", "GET"]}))
 app.listen(3000, () => console.log("listening on port 3000"))
 
 app.get("/", (req, res) => {
@@ -21,12 +28,9 @@ app.get("/new", (req, res) => {
   res.render("new")
 })
 
-app.post("/urls", (req, res) => {
-  Url.create({
-    long: req.body.long,
-    short: checksum(req.body.long).substr(0, 5)
-  }).then(url => {
-    res.redirect(`/urls/${url._id}`)
+app.delete("/urls/:id", (req, res) => {
+  Url.remove({_id: req.params.id}).then(() => {
+    res.redirect("/")
   })
 })
 
@@ -38,6 +42,32 @@ app.get("/urls/:id", (req, res) => {
     })
   })
 })
+
+app.post("/urls", (req, res) => {
+  Url.create({
+    long: req.body.long,
+    short: checksum(req.body.long).substr(0, 5)
+  }).then(url => {
+    res.redirect(`/urls/${url._id}`)
+  })
+})
+
+app.get("/urls/:id/edit", (req, res) => {
+  Url.findOne({_id: req.params.id}).then(url => {
+    res.render("edit", {url})
+  })
+})
+
+app.put("/urls/:id", (req, res) => {
+  Url.findOneAndUpdate({_id: req.params.id}, {
+    long: req.body.long,
+    short: checksum(req.body.long).substr(0, 5)
+  }, {new: true}).then(url => {
+    res.redirect(`/urls/${url._id}`)
+  })
+})
+
+// Non-RESTful
 
 app.get("/:short", (req, res) => {
   Url.findOne({short: req.params.short}).then(url => {

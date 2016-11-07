5d59b8a1bb7ecb7a269141feb0058fb8df5880b5
RYANBROPHY <BROPHY2321@ME.COM>
2016-11-07 15:53:43 -0500

    part 1

---
+++ db/seeds.js
@@ -0,0 +1,7 @@
+var Url = require("./models");
+
+
+
+Url.create({short: "http://short.com", long: "http://verylongaddress.com/stuff.html", timestamp: Date.now() }).then(url => {
+  console.log(url)
+})
